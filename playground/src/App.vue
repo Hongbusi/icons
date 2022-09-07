361b9a96a551207c523e3fab2bdcfcbf4256f3ce
Hongbusi <coderhbs@gmail.com>
2022-09-07 11:10:25 +0800

    chore: playground

---
+++ playground/src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { icons } from '@hongbusi/icons-vue/global'
+</script>
+
+<template>
+  <component :is="Icon" v-for="(Icon, key) in icons" :key="key" class="icon" />
+  <hr>
+  <component
+    :is="`HIcon${key}`"
+    v-for="key in Object.keys(icons)"
+    :key="key"
+    class="icon"
+  />
+</template>
+
+<style>
+.icon {
+  height: 48px;
+  color: #409eff;
+}
+</style>
---
+++ playground/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import HIcons from '@hongbusi/icons-vue/global'
+import App from './App.vue'
+
+createApp(App).use(HIcons).mount('#app')
